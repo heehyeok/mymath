d1d2a45be206c49cfaa7f176317e6a590144d349
heehyeok <ghdgmlgur0514@gmail.com>
2025-07-14 16:12:25 +0900

    Add godoc comments

--- add.go
+++ add.go
@@ -1,5 +1,10 @@
+// Package mymath provides basic math utilities.
 package mymath
 
+// Add add two integers and returns the result.
+//
+//For more info on addition, see:
+//https:www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int {
 	return a + b
 }
